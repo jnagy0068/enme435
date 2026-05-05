845ce39cafe086303fd647140148852879e53962
jnagy0068 <53166267+jnagy0068@users.noreply.github.com>
2026-05-05 16:32:41 -0400

    Update data_collection2.py

--- bird/data_collection2.py
+++ bird/data_collection2.py
@@ -1,5 +1,4 @@
-data_collection2.py# ENME489Y: Remote Sensing
-
+# ENME489Y: Remote Sensing
 # import the necessary packages
 import numpy as np
 import time
@@ -9,34 +8,37 @@ import os
 # allow the camera to setup
 time.sleep(1)
 
-# Enter the initial IMU angle from user
-d = input("Please enter IMU angle: ")
-print("Confirming the IMU angle you entered is: " + d)
+while True:
+    # Enter the initial IMU angle from user
+    d = input("Please enter IMU angle (or 'q' to quit): ")
+    
+    if d.lower() == 'q':
+        print("Exiting. All done!")
+        break
 
-# The following line should be used if interestsed in
-# the -ss ("shutter speed") command for long exposure
-#command = 'raspistill -w 1280 -h 720 -ss 1000000 -o blank.jpg'
-command = 'libcamera-still -o blank.jpg --width 1280 --height 720 --shutter 12000000 --contrast 1.0'
-os.system(command)
+    print("Confirming the IMU angle you entered is: " + d)
 
-image = cv2.imread('blank.jpg')
-image = cv2.flip(image,-1)
+    command = 'libcamera-still -o blank.jpg --width 1280 --height 720 --shutter 12000000 --contrast 1.0'
+    os.system(command)
 
-# plot crosshairs for alignment
-cv2.line(image, (640,0), (640,720), (0,150,150), 1)
-cv2.line(image, (600,360), (1280,360), (0,150,150), 1)
+    image = cv2.imread('blank.jpg')
+    image = cv2.flip(image, -1)
 
-# display IMU angle, for reference
-font = cv2.FONT_HERSHEY_COMPLEX_SMALL
-red = (0, 0, 255)
-cv2.putText(image, d, (800, 200), font, 10, red, 10)
+    # plot crosshairs for alignment
+    cv2.line(image, (640, 0), (640, 720), (0, 150, 150), 1)
+    cv2.line(image, (600, 360), (1280, 360), (0, 150, 150), 1)
 
-# write image to file
-d = int(d)
-filename = "%d.jpg" %d
-cv2.imwrite(filename, image)
+    # display IMU angle, for reference
+    font = cv2.FONT_HERSHEY_COMPLEX_SMALL
+    red = (0, 0, 255)
+    cv2.putText(image, d, (800, 200), font, 10, red, 10)
 
-command = 'mv ' + filename + ' /home/pi/alignment_images/'
-os.system(command)
+    # write image to file
+    d = int(d)
+    filename = "%d.jpg" % d
+    cv2.imwrite(filename, image)
 
-print("All done!")
+    command = 'mv ' + filename + ' /home/pi/alignment_images/'
+    os.system(command)
+
+    print(f"Image saved for angle {d}. Ready for next capture.")
